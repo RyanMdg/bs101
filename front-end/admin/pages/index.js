861a2eba636d5792405cf55fc8bdca4bbef34287
RyanMdg <ryandeguia.dev@gmail.com>
2023-09-30 07:14:27 +0800

    testing

--- front-end/admin/pages/index.js
+++ front-end/admin/pages/index.js
@@ -3,7 +3,7 @@ import "../src/app/globals.css";
 const Home = () => {
   return (
     <main>
-      <h1 className=" bg-red-600">Bs101</h1>
+      <h1 className=" bg-[#3B82F6] text-white p-6">Bs101</h1>
     </main>
   );
 };
