fe2d2de1840b745a7f14824ad98853cf5f5f048d
RyanMdg <ryandeguia.dev@gmail.com>
2023-09-30 18:38:49 +0800

    comprog reviewer sec

--- front-end/admin/src/app/components/comprog-review.js
+++ front-end/admin/src/app/components/comprog-review.js
@@ -15,8 +15,26 @@ const comprogReviewer = () => {
       <h1 className=" font-semibold mt-5">
         <b className=" underline">High-level Languages</b> – These are the
         programming languages that use natural languages, such as the English
-        language.
+        language.high-level language has its own syntax.
       </h1>
+      <ul className=" flex flex-col gap-3 list-inside list-disc ps-2 mt-3 font-semibold">
+        {" "}
+        <li>
+          <b className=" underline bg-yellow-400 px-3">Syntax </b> – rules of
+          the language, for example, print or write is used to produce an output
+        </li>
+        <li>
+          <b className=" underline bg-yellow-400 px-3">Commands</b> – these are
+          program statements that carry out tasks that the program has to
+          perform
+        </li>
+        <li>
+          <b className=" underline bg-yellow-400 px-3">Compiler</b> –translates
+          a program written in a high-level language into a low-level language
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 };
