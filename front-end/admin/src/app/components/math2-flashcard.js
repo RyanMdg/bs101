3024b486176ca65a2fbb87705823b8eb4c828571
JLSed <seddd001@gmail.com>
2023-09-30 21:15:51 +0800

    math flashcard first-commit test

---
+++ front-end/admin/src/app/components/math2-flashcard.js
@@ -0,0 +1,9 @@
+const mathFlashCards = () => {
+    return(
+        <div>
+            <p className=" text-center">A sequences or designs that are orderly and that repeat</p>
+        </div>
+    );
+};
+
+export default mathFlashCards;
