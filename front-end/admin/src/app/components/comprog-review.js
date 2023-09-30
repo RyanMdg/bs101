40432491aebf3bf3f202b3cd8b9c95d63e0232cf
RyanMdg <ryandeguia.dev@gmail.com>
2023-09-30 18:27:52 +0800

    comprog review sec

--- front-end/admin/src/app/components/comprog-review.js
+++ front-end/admin/src/app/components/comprog-review.js
@@ -6,6 +6,17 @@ const comprogReviewer = () => {
         statements intended to accomplish a certain task. It is a set of
         instructions for a computer to follow.
       </h1>
+
+      <h1 className=" font-semibold mt-5">
+        <b className=" underline">Programming</b> – It is a process of planning
+        and creating a program.
+      </h1>
+
+      <h1 className=" font-semibold mt-5">
+        <b className=" underline">High-level Languages</b> – These are the
+        programming languages that use natural languages, such as the English
+        language.
+      </h1>
     </div>
   );
 };
