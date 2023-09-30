6c9b6d0fd37da3da52df0fd179fbd0753feead9a
RyanMdg <ryandeguia.dev@gmail.com>
2023-09-30 14:03:15 +0800

    try some updates

--- front-end/admin/src/app/components/subjects.js
+++ front-end/admin/src/app/components/subjects.js
@@ -1,13 +1,13 @@
 const subject = () => {
   return (
     <div className=" mt-8 md:grid grid-cols-2 gap-4 mx-4">
-      <a href="math">
-        <img src="/math2.png" className=" rounded-md" alt="" /> Math In Modern
-        world
+      <a href="math" className="flex flex-col items-center ">
+        <img src="/math2.png" className=" rounded-md" alt="" />
+        <span className=" mt-2">Math In Modern world</span>
       </a>
-      <a href="nstp">
-        <img src="/nstp.png" className=" h-[70%] rounded-md" alt="" />
-        Nstp
+      <a href="nstp" className=" flex flex-col items-center">
+        <img src="/nstp.png" className=" h-[70%] mt-7 rounded-md" alt="" />
+        <span className=" mt-2">Nstp</span>
       </a>
     </div>
   );
