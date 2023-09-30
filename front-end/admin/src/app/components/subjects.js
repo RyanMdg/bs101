d44d16559cf71148c7b870c0cf984c7e4ae01be8
RyanMdg <ryandeguia.dev@gmail.com>
2023-09-30 13:57:42 +0800

    fixing grids

--- front-end/admin/src/app/components/subjects.js
+++ front-end/admin/src/app/components/subjects.js
@@ -1,11 +1,12 @@
 const subject = () => {
   return (
-    <div className=" mt-8 grid grid-cols-2 gap-4 mx-4">
+    <div className=" mt-8 md:grid grid-cols-2 gap-4 mx-4">
       <a href="math">
-        <img src="/math2.png" alt="" /> Math In Modern world
+        <img src="/math2.png" className=" rounded-md" alt="" /> Math In Modern
+        world
       </a>
       <a href="nstp">
-        <img src="/nstp2.jpg" className=" h-[63%] w-full" alt="" />
+        <img src="/nstp.png" className=" h-[70%] rounded-md" alt="" />
         Nstp
       </a>
     </div>
