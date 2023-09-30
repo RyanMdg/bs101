b4be5b7aafa4ab53e0a305b2a27dc02f10d8330b
JLSed <seddd001@gmail.com>
2023-09-30 21:49:03 +0800

    math flashcard edit

--- front-end/admin/src/app/components/math2-flashcard.js
+++ front-end/admin/src/app/components/math2-flashcard.js
@@ -1,8 +1,8 @@
 const mathFlashCards = () => {
     return (
-        <div className="mx-2 mt-5">
-            <h1 className="text-4x1 text-center">Pattern</h1>
-            <p className="text-4x1 text-center font-medium">A sequences or designs that are orderly and that repeat</p>
+        <div className="mx-auto mt-5">
+            <h1 className="text-4x1 font-extrabold">Pattern</h1>
+            <p className="text-2x1 font-medium">A sequences or designs that are orderly and that repeat</p>
         </div>
     );
 };
