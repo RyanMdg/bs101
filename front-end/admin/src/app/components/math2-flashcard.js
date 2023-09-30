ba05b2a50300b1755c7750ef2fdad985bd2f70f3
JLSed <seddd001@gmail.com>
2023-09-30 22:39:33 +0800

    math flashcard edit

--- front-end/admin/src/app/components/math2-flashcard.js
+++ front-end/admin/src/app/components/math2-flashcard.js
@@ -1,16 +1,29 @@
 const mathFlashCards = () => {
     return (
         <div className="container mx-auto mt-5">
-            <p class="text-5x1 font-extrabold">Pattern</p>
-            <p class="text-2x1 font-medium">A sequences or designs that are orderly and that repeat</p>
-            {" "}
-            <p className="text-5x1 font-extrabold">Pattern</p>
-            <p className="text-2x1 font-medium">A sequences or designs that are orderly and that repeat</p>
-            {" "}
-            <p className="text-5x1 font-extrabold">Pattern</p>
-            <p className="text-2x1 font-medium">A sequences or designs that are orderly and that repeat</p>
-            {" "}
+            <p class="text-5xl font-extrabold">Pattern</p>
+            <p class="text-2xl font-medium">A sequences or designs that are orderly and that repeat</p>
+           
+            <p className="text-5xl font-extrabold">Symmetry</p>
+            <p className="text-2xl font-medium">When different sides of something are alike. These reflections may be mirror images with only two sides like the two sides of our bodies</p>
+           
+            <p className="text-5xl font-extrabold">Fractal</p>
+            <p className="text-2xl font-medium"> the 'never-ending' patterns that repeat indefinitely as the pattern is iterated on an infinitely smaller scale.</p>
             
+            <p className="text-5xl font-extrabold">Spiral</p>
+            <p className="text-2xl font-medium"> another common pattern in nature that we see more often in living things. We see this pattern in hurricanes, galaxies, and some seashells.</p>
+            
+            <p className="text-5xl font-extrabold">Fibonacci Sequence</p>
+            <p className="text-2xl font-medium"> the sequence of numbers that each number is the sum of the two numbers before it</p>
+            
+            <p className="text-5xl font-extrabold">Fibonacci Pattern</p>
+            <p className="text-2xl font-medium"> We see that some plants exhibit this pattern, like the branches of a tree.You start with the main branch at the bottom. It splits off so that you have two, and it splits off again so that you have 3, and so forth.</p>
+            
+            <p className="text-5xl font-extrabold">Language</p>
+            <p className="text-2xl font-medium">a complex system of words and symbols, either spoken or written, used by a particular community as a means of communication.</p>
+
+            <p className="text-5xl font-extrabold">Fibonacci Pattern</p>
+            <p className="text-2xl font-medium"> We see that some plants exhibit this pattern, like the branches of a tree.You start with the main branch at the bottom. It splits off so that you have two, and it splits off again so that you have 3, and so forth.</p>
         </div>
     );
 };
