354eb9fd460f719e1285867ebdd3eb2404c1128a
JLSed <seddd001@gmail.com>
2023-10-01 22:40:44 +0800

    added hard coded math flashcards

--- main/front-end/admin/src/app/components/math2-flashcard.js
+++ main/front-end/admin/src/app/components/math2-flashcard.js
@@ -1,76 +1,160 @@
-const mathFlashCards = () => {
+const MathFlashCards = () => {
+
   return (
-    <div className="container mx-auto mt-5">
-      <p class="text-2xl font-extrabold">Pattern</p>
-      <p class="mb-10 text-xl font-medium">
-        A sequences or designs that are orderly and that repeat
-      </p>
+      <div className="container mx-auto">
 
-      <p className="text-2xl font-extrabold">Symmetry</p>
-      <p className="mb-10 text-xl font-medium">
-        When different sides of something are alike. These reflections may be
-        mirror images with only two sides like the two sides of our bodies
-      </p>
-
-      <p className="text-2xl font-extrabold">Fractal</p>
-      <p className="mb-10 text-xl font-medium">
-        {" "}
-        the 'never-ending' patterns that repeat indefinitely as the pattern is
-        iterated on an infinitely smaller scale.
-      </p>
-
-      <p className="text-2xl font-extrabold">Spiral</p>
-      <p className="mb-10 text-xl font-medium">
-        {" "}
-        another common pattern in nature that we see more often in living
-        things. We see this pattern in hurricanes, galaxies, and some seashells.
-      </p>
-
-      <p className="text-2xl font-extrabold">Fibonacci Sequence</p>
-      <p className="mb-10 text-xl font-medium">
-        {" "}
-        the sequence of numbers that each number is the sum of the two numbers
-        before it
-      </p>
-
-      <p className="text-2xl font-extrabold">Fibonacci Pattern</p>
-      <p className="mb-10 text-xl font-medium">
-        {" "}
-        We see that some plants exhibit this pattern, like the branches of a
-        tree.You start with the main branch at the bottom. It splits off so that
-        you have two, and it splits off again so that you have 3, and so forth.
-      </p>
-
-      <p className="text-2xl font-extrabold">Language</p>
-      <p className="mb-10 text-xl font-medium">
-        a complex system of words and symbols, either spoken or written, used by
-        a particular community as a means of communication.
-      </p>
-
-      <p className="text-2xl font-extrabold">Precise</p>
-      <p className="mb-10 text-xl font-medium">
-        able to make very fine distinctions
-      </p>
-
-      <p className="text-2xl font-extrabold">Concise</p>
-      <p className="mb-10 text-xl font-medium">able to say things briefly</p>
-
-      <div className="flex min-h-screen flex-col justify-center select-none">
-        <div className="group h-96 w-100 [perspective:1000px]">
-          <div className=" relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
-            <div className="text-center absolute inset-0 mt-10 text-2xl font-medium">
-              <p>able to express complex thoughts with relative ease</p>
-            </div>
-            <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-black group-active:[backface-visibility:visible]">
-              <div className="text-slate-100 text-center text-6xl font-extrabold">
-                <p className=" mt-20">Powerful</p>
+          <div className="flex min-h-fit mb-20 mt-10 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:500px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className="mt-10 px-5">A sequences or designs that are orderly and that repeat.</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className = "mt-20">Pattern</p>
+                          </div>    
+                      </div>
+                  </div>
               </div>
-            </div>
           </div>
-        </div>
+         
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:500px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className="px-5">When different sides of something are alike. These reflections may be mirror images with only two sides like the two sides of our bodies.</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="mt-20 text-slate-100 text-center text-5xl font-extrabold">
+                              <p className = "mt-20">Symmetry</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:500px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className="px-5">the 'never-ending' patterns that repeat indefinitely as the pattern is iterated on an infinitely smaller scale.</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className = "mt-20">Fractal</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:500px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className="px-5">another common pattern in nature that we see more often in living things. We see this pattern in hurricanes, galaxies, and some seashells.</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className = "mt-20">Spiral</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:500px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className="px-5">the sequence of numbers that each number is the sum of the two numbers before it.</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className = "mt-20">Fibonacci Sequence</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:500px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-xl">
+                          <p className="px-5"> We see that some plants exhibit this pattern, like the branches of a tree. You start with the main branch at the bottom. It splits off so that you have two, and it splits off again so that you have 3, and so forth.</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className = "mt-20">Fibonacci Pattern</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:1000px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className="px-5">a complex system of words and symbols, either spoken or written, used by a particular community as a means of communication.</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className = "mt-20">Language</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:1000px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className = "mt-20 px-5">able to make very fine distinctions</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className=" mt-20">Precise</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:1000px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className = "mt-20 px-5">able to say things briefly</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className="mt-20">Concise</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="flex min-h-fit mb-20 flex-col justify-center select-none">
+              <div className="group h-60 w-100 [perspective:1000px]">
+                  <div className="bg-slate-100 relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d group-active:[transform:rotateY(180deg)] ">
+                      <div className="text-center absolute inset-0 text-2xl font-medium">
+                          <p className = "mt-20 px-5">able to express complex thoughts with relative ease</p>
+                      </div>
+                      <div className="absolute inset-0 h-full w-full rounded-xl px-12 [transform:rotateY(180deg)] transition-all [backface-visibility:hidden] group-active:bg-blue-400 group-active:[backface-visibility:visible]">
+                          <div className="text-slate-100 text-center text-6xl font-extrabold">
+                              <p className=" mt-20">Powerful</p>
+                          </div>    
+                      </div>
+                  </div>
+              </div>
+          </div>
+
       </div>
-    </div>
   );
 };
 
-export default mathFlashCards;
+export default MathFlashCards;
