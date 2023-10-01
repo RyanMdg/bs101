6f8e0dc4454d8c70974aaeec13385ecdc0463f85
JLSed <seddd001@gmail.com>
2023-10-01 19:19:20 +0800

    added more flashcards for math page

--- front-end/admin/pages/math2.js
+++ front-end/admin/pages/math2.js
@@ -2,6 +2,7 @@ import Navbar from "../src/app/components/navbar";
 import MathFlashCards from "../src/app/components/math2-flashcard";
 
 const math = () => {
+
   return (
     <div>
       <Navbar />
