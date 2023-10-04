f2aa72f1da9108a6ac5e4de499218d337f0fb17e
RyanMdg <ryandeguia.dev@gmail.com>
2023-10-04 21:32:27 +0800

    updating designs & adding ramdom flashcards

--- main/front-end/admin/src/app/components/comprog-review.js
+++ main/front-end/admin/src/app/components/comprog-review.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../globals.css";
+
+function shuffleArray(array) {
+  const shuffledArray = [...array];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+}
 
 const ComprogReviewer = () => {
   const [flashcards, setFlashcards] = useState([]);
@@ -19,24 +29,19 @@ const ComprogReviewer = () => {
             flashcard.subjects === "Computer Programming" &&
             flashcard.handouts === 1
         );
-        setFlashcards(computerProgrammingFlashcards);
-      })
-      .catch((error) => {
-        console.error("Error fetching flashcards:", error);
-      });
-  }, []);
 
-  useEffect(() => {
-    axios
-      .get("https://flashcardsbs101.onrender.com/flashcard/getFlashCard")
-      .then((response) => {
-        // Filtering comprog sub
         const computerProgrammingHandouts = response.data.filter(
           (handouts) =>
             handouts.handouts === 2 &&
             handouts.subjects === "Computer Programming"
         );
-        setHandouts(computerProgrammingHandouts);
+
+        // random fetch
+        const shuffledFlashcards = shuffleArray(computerProgrammingFlashcards);
+        const shuffledHandouts = shuffleArray(computerProgrammingHandouts);
+
+        setFlashcards(shuffledFlashcards);
+        setHandouts(shuffledHandouts);
       })
       .catch((error) => {
         console.error("Error fetching flashcards:", error);
@@ -64,7 +69,7 @@ const ComprogReviewer = () => {
     <div>
       <h2 className="text-center mt-4 uppercase font-extrabold">Midterms</h2>
       <br />
-      <div className="mx-7 pb-7 mt-5 bg-[#FFF7F0]  shadow-2xl rounded-lg pt-5 px-3">
+      <div className="mx-7 pb-7 mt-5 bg-[#f7eee6]  shadow-2xl rounded-lg pt-5 px-3">
         <h1 className="uppercase font-extrabold">Handout 1</h1>
         <ul className="list-inside list-disc">
           <li className="font-semibold underline">
@@ -82,10 +87,10 @@ const ComprogReviewer = () => {
             &#8250;
           </button>
           <div
-            className={`w-full px-[2rem] h-40 p-4 bg-[#3B82F6] flex items-center justify-center rounded-lg shadow-md cursor-pointer`}
+            className={`w-full h-auto max-h-40 p-4 bg-[#3B82F6] inner-shadow flex items-center justify-center rounded-lg shadow-md cursor-pointer`}
             onClick={flipCard}
           >
-            <p className="text-[1rem] text-white font-semibold">
+            <p className="text-[1rem] px-1 overflow-auto text-white font-semibold">
               {showAnswer
                 ? flashcards[currentIndex]?.answer
                 : flashcards[currentIndex]?.question}
@@ -94,7 +99,7 @@ const ComprogReviewer = () => {
         </div>
       </div>
 
-      <div className="mx-7 pb-7 mt-5 bg-[#FFF7F0]  shadow-2xl rounded-lg pt-5 px-3">
+      <div className="mx-7 pb-7 mt-5 bg-[#f7eee6]  shadow-2xl rounded-lg pt-5 px-3">
         <h1 className="uppercase font-extrabold">Handout 2</h1>
         <ul className="list-inside list-disc">
           <li className="font-semibold underline">
@@ -110,11 +115,12 @@ const ComprogReviewer = () => {
           >
             &#8250;
           </button>
+
           <div
-            className={`w-full px-[2rem] h-40 p-4 bg-[#3B82F6] flex items-center justify-center rounded-lg shadow-md cursor-pointer`}
+            className={`w-full h-auto max-h-40 p-4 bg-[#3B82F6] inner-shadow flex items-center justify-center rounded-lg shadow-md cursor-pointer`}
             onClick={flipCard1}
           >
-            <p className="text-[1rem] text-white font-semibold">
+            <p className="text-[1rem] px-1 overflow-auto text-white font-semibold">
               {showAnswer1
                 ? handouts[currentIndex1]?.answer
                 : handouts[currentIndex1]?.question}
